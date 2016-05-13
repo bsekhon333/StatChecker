c4d5f3d2fdcbdf9ba383973fb21054117c2cb442
Bikram <bikram@Bikrams-MacBook-Pro.local>
2016-05-12 18:37:12 -0700

    directive didnt show last time still

--- directives/footer.directive.js
+++ directives/footer.directive.js
@@ -3,7 +3,7 @@ angular.module('myApp.directives.footer', [])
     return {
       restrict:'E',
       transclude:true,
-      templateUrl:"/templates/myAwesomeFooter.html",
+      templateUrl:"templates/myAwesomeFooter.html",
       controller: function($scope){
         console.log("hi");
       }
